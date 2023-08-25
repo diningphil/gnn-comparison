66ee20111e42b68dc8fb81a13e2fb0d920f43761
SamirMoustafa <samir.moustafa.97@gmail.com>
2023-08-25 16:22:33 +0200

    update the install.sh, rollback requirements.txt and remove environment.yaml

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #
 # Copyright (C)  2020  University of Pisa
+# Copyright (C)  2023  University of Vienna
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,17 +15,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# default pytorch version is 1.4.0
-PYTORCH_VERSION=2.0.1
+# Find the path to the Conda binary
+CONDA_BIN_PATH=$(which conda)
+
+# Extract the path to the Conda installation
+CONDA_DIR_PATH=$(dirname $(dirname $CONDA_BIN_PATH))
+
+# Source the Conda initialization script
+source $CONDA_DIR_PATH/etc/profile.d/conda.sh
+
+# default pytorch version is 2.0.1
+# available options are 1.4.0 and 2.0.1
+PYTORCH_VERSION=${1:-"2.0.1"}
 
 # set CUDA variable (defaults to cpu if no argument is provided to the script)
-# available options for for pytorch 1.4.0 are cpu, cu92, cu100, cu101
-CUDA_VERSION=11.7.0
+# available options for pytorch 2.0.1 are cpu, and cu117
+# available options for pytorch 1.4.0 are cpu, cu92, cu100, and cu101
+CUDA_VERSION=${2:-"cpu"}
+
 
 # create virtual environment and activate it
-conda create --name gnn-comparison python=3.10.9 -y
+conda create --name gnn-comparison python=3.10 -y
 conda activate gnn-comparison
 
+# install requirements
+pip install -r requirements.txt
+
 # install pytorch
-conda install -c "nvidia/label/cuda-${CUDA_VERSION}" cuda-toolkit
-conda install -c pytorch pytorch
+case "$CUDA_VERSION" in
+  "cpu")
+    conda install pytorch==${PYTORCH_VERSION} cpuonly -c pytorch -y
+    ;;
+  "cu92")
+    conda install pytorch==${PYTORCH_VERSION} cudatoolkit=9.2 -c pytorch -y
+    ;;
+  "cu100")
+    conda install pytorch==${PYTORCH_VERSION} cudatoolkit=10.0 -c pytorch -y
+    ;;
+  "cu101")
+    conda install pytorch==${PYTORCH_VERSION} cudatoolkit=10.1 -c pytorch -y
+    ;;
+  "cu117")
+    conda install -c "nvidia/label/cuda-${CUDA_VERSION}" cuda-toolkit
+    conda install pytorch==${PYTORCH_VERSION} cudatoolkit=11.7.0 -c pytorch -y
+    ;;
+  *)
+    echo "Error: Unsupported CUDA version: ${CUDA_VERSION}. Exiting."
+    exit 1
+    ;;
+esac
+
+# install torch-geometric
+pip install torch-geometric
