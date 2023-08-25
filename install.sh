bbe826fe6db4cc63c8092b964260c74c375154ff
SamirMoustafa <samir.moustafa.97@gmail.com>
2023-08-25 17:49:09 +0200

    fix the copying rights typo

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #
-# Copyright (C)  2020  University of Pisa
-# Copyright (C)  2023  University of Vienna
+# Copyright (C) 2023 University of Pisa, University of Vienna
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
